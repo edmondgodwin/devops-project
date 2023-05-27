f4f4879f04162a2a90dd0cfab07246e7c283bbca
Edmond Godwin <edmondmgodwin@outlook.com>
2023-05-27 05:25:59 +0100

    Created script for adding pub key to servers

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,3 +17,31 @@ output "public_ips" {
   value = { for instance_name, instance in aws_instance.ec2_instances : instance_name => instance.public_ip }
 }
 
+output "private_ips" {
+  value = { for instance_name, instance in aws_instance.ec2_instances : instance_name => instance.private_ip }
+}
+
+resource "aws_instance" "ansible" {
+  ami = var.ami_id
+  instance_type = var.instance_type
+  key_name = var.key_pair
+  security_groups = var.security_group
+  root_block_device {
+    volume_size = var.volume_size_gb
+  } 
+  tags = {
+    Name = "ansible_master"
+  }
+  user_data = <<-EOF
+            #!/bin/bash
+            sudo yum update -y
+            sudo yum install -y python3.7
+            sudo yum install -y python3-pip
+            python3 -m pip install --user --upgrade pip
+            python3 -m pip install ansible
+            EOF
+}
+
+output "ansible_publicip" {
+  value = aws_instance.ansible.public_ip
+}
